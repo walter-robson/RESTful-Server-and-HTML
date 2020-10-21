69f28294a4cc475203d3690988b8f1c31c52da8c
Walter Robson <wrobson@nd.edu>
2020-10-21 20:24:48 +0000

    Update main.js

--- Daily 14/scripts/main.js
+++ Daily 14/scripts/main.js
@@ -33,9 +33,46 @@ function makeRequest(){
             action = "DELETE";
     }
 
+    console.log('Beginning to make nw call' + name);
+    // set up url
+    var xhr = new XMLHttpRequest(); // 1 - creating request object
+    var url = null;
+    if(key != null){
+        url = url_base + ':' + port + '/' + key + '/' + message_body;
+    } else {
+        var url = url_base + ':' + port + '/' + message_body;
+    }
     
+    xhr.open(action, url, true); // 2 - associates request attributes with xhr
+
+    // set up onload
+    xhr.onload = function(e) { // triggered when response is received
+        // must be written before send
+        console.log(xhr.responseText);
+        // do something
+        updateResponse(xhr.responseText);
+    }
+
+    // set up onerror
+    xhr.onerror = function(e) { // triggered when error response is received and must be before send
+        console.error(xhr.statusText);
+    }
+
+    // actually make the network call
+    xhr.send(null) // last step - this actually makes the request
 
 
+
+};
+
+function updateResponse(response){
+        var response_label = document.getElementById('response-label');
+        response_label.innerHTML = response;
+
+        var response_json = JSON.parse(response);
+
+        var answer_label = document.getElementById('answer-label');
+        answer_label.innerHTML = response_json['title'] + " belongs to the genres " + response_json['genres'];
 };
 
 
