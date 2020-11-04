c3b5bc161e4c05833c27d6c1bc05c809943b13c1
Walter Robson <wrobson@nd.edu>
2020-11-04 03:22:06 +0000

    Update main.js

--- Final-Project/scripts/main.js
+++ Final-Project/scripts/main.js
@@ -45,8 +45,8 @@ function makeRequest(){
     } else {  
         url = url_base + ':' + port + '/dictionary/';
     }
-    
-    xhr.open("GET", url, true); // 2 - associates request attributes with xhr
+    var action = "GET"
+    xhr.open(action, url, true); // 2 - associates request attributes with xhr
 
     // set up onload
     xhr.onload = function(e) { // triggered when response is received
