f1e19e8a10fb281578f6d1eb8dbce0c25f4e955e
Walter Robson <wrobson@nd.edu>
2020-11-05 02:40:45 +0000

    Update comment.js

--- Final-Project/scripts/comment.js
+++ Final-Project/scripts/comment.js
@@ -45,7 +45,7 @@ function makeRequest(){
     } else {*/
      xhr.send(message_body) // last step - this actually makes the request
   //  }
-   
+   makeCommentRequest(name);
 };
 
 function updateResponse(response){
@@ -58,4 +58,58 @@ function updateResponse(response){
         } else {
         answer_label.innerHTML = response_json['result'] ;
         }
+
+};
+function makeCommentRequest(name){
+    console.log("Entered Comments!")
+    // get text from title, author and story
+    const url_base = 'http://student04.cse.nd.edu';
+    const port = '51086';
+
+    console.log('Beginning to make nw call' + name);
+    
+    // set up url
+    var xhr = new XMLHttpRequest(); // 1 - creating request object
+    var url = null;
+
+    if(name != null){
+        url = url_base + ':' + port + '/comments/' + name;   
+    } else {  
+        updateResponse("{'result': 'error'}");
+    }
+    var action = "GET"
+    xhr.open(action, url, true); // 2 - associates request attributes with xhr
+
+    // set up onload
+    xhr.onload = function(e) { // triggered when response is received
+        // must be written before send
+        console.log(xhr.responseText);
+        // do something
+        updateComments(xhr.responseText);
+    }
+    console.log(url);
+    // set up onerror
+    xhr.onerror = function(e) { // triggered when error response is received and must be before send
+        console.error(xhr.statusText);
+    }
+
+    // actually make the network call
+   /* if(message_body != null) {
+        xhr.send(message_body)
+    } else {*/
+     xhr.send(null) // last step - this actually makes the request
+  //  }
+   
+};
+function updateComments(response){
+        
+        var response_json = JSON.parse(response);
+
+        var answer_label = document.getElementById('comment-label');
+        if(response_json['output'] === 'error'){
+        answer_label.innerHTML = "ERROR"
+        } else {
+        answer_label.innerHTML = response_json['comment'] ;
+        }
+
 };
